29a317a559f7e27e4fb31efe1488bc8c3536b9cc
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2022-09-24 23:23:12 +0200

    Major Update
    Rebuilt the project to allow for better customization and features as well as a more complete solution in the future

--- index.js
+++ index.js
@@ -1,119 +1,19 @@
-const localStorageContent = JSON.parse(localStorage.getItem("data"));
+const setTableNumber = () => {
+  const tableNumberElement = document.getElementById("table-number");
 
-let objectsForTable = localStorageContent;
-
-function initVariables() {
-  let keys = [];
-  if (localStorageContent != null) {
-    if (localStorageContent.length != 0) {
-      for (i = 0; i < localStorageContent.length; i++) {
-        for (j = 0; j < Object.keys(localStorageContent[i]).length; j++) {
-          console.log(Object.keys(localStorageContent[i])[j]);
-          // This verification prevent key duplications
-          if (!keys.some((e) => e === Object.keys(localStorageContent[i])[j])) {
-            keys.push(Object.keys(localStorageContent[i])[j]);
-          }
-        }
-      }
-    }
+  let tableNumber = 0;
+  if (localStorage.getItem("tables").length) {
+    tableNumber = JSON.parse(localStorage.getItem("tables")).length;
   }
 
-  return keys;
-}
+  tableNumberElement.textContent = tableNumber;
+};
 
-const tableColumns = initVariables();
+const setUserName = () => {
+  const usernameElement = document.getElementById("username");
 
-let state = 0;
-function createTable(data, sortBy) {
-  sortTable(sortBy);
+  usernameElement.textContent = "invité";
+};
 
-  const table = document.createElement("table");
-  table.setAttribute("id", "table");
-  const thead = document.createElement("thead");
-  const tr = document.createElement("tr");
-  tr.setAttribute("id", "tableColumnsContainer");
-  for (i = 0; i < tableColumns.length; i++) {
-    const th = document.createElement("th");
-    th.setAttribute("id", tableColumns[i] + "sort");
-    th.setAttribute("class", "table-column");
-    th.innerText = tableColumns[i];
-
-    const sortImage = document.createElement("img");
-    sortImage.setAttribute("id", tableColumns[i] + "sortImage");
-    sortImage.setAttribute("src", "./public/assets/img/sort-svgrepo-com.svg");
-    sortImage.setAttribute("width", "14px");
-    th.appendChild(sortImage);
-
-    tr.appendChild(th);
-    tr.addEventListener("click", (e) => {
-      const sortBy = e.target.innerText;
-      document.getElementById("table-container").innerHTML = "";
-      createTable(objectsForTable, sortBy);
-    });
-  }
-  thead.appendChild(tr);
-  table.appendChild(thead);
-  const tbody = document.createElement("tbody");
-  for (i = 0; i < data.length; i++) {
-    const tr = document.createElement("tr");
-    for (j = 0; j < tableColumns.length; j++) {
-      const td = document.createElement("td");
-      if (data[i][tableColumns[j]] != null) {
-        td.innerText = data[i][tableColumns[j]];
-      } else {
-        td.innerText = "/";
-      }
-
-      tr.appendChild(td);
-    }
-    tbody.appendChild(tr);
-  }
-  table.appendChild(tbody);
-
-  document.getElementById("table-container").appendChild(table);
-}
-
-createTable(objectsForTable, tableColumns[0]);
-
-function searchEntries() {
-  let value = document.getElementById("searchbar").value;
-  if (value == "") {
-    objectsForTable = localStorageContent;
-  } else {
-    objectsForTable = localStorageContent.filter((entry) => {
-      return Object.keys(entry).some((key) =>
-        entry[key].toLowerCase().includes(value.toLowerCase())
-      );
-    });
-
-    console.log(objectsForTable);
-  }
-  document.getElementById("table-container").innerHTML = "";
-  createTable(objectsForTable);
-}
-function sortTable(keyToSort, previous) {
-  console.log("prevous = " + previous + " and now = " + keyToSort);
-  if (state == 0) {
-    state = 1;
-    return objectsForTable.sort((a, b) => {
-      if (a[keyToSort] < b[keyToSort]) {
-        return -1;
-      }
-      if (a[keyToSort] > b[keyToSort]) {
-        return 1;
-      }
-      return 0;
-    });
-  } else {
-    state = 0;
-    return objectsForTable.sort((a, b) => {
-      if (a[keyToSort] < b[keyToSort]) {
-        return 1;
-      }
-      if (a[keyToSort] > b[keyToSort]) {
-        return -1;
-      }
-      return 0;
-    });
-  }
-}
+setUserName();
+setTableNumber();
---
+++ table/index.js
@@ -0,0 +1,328 @@
+const newObjectForm = document.getElementById("newObjectForm");
+
+let tables = [];
+
+if (localStorage.getItem("tables")) {
+  tables = JSON.parse(localStorage.getItem("tables"));
+} else {
+  tables = localStorage.setItem("tables", tables);
+}
+
+function initPage() {
+  const selectSection = document.getElementById("selector-select-new");
+  console.log(tables);
+
+  if (Array.isArray(tables)) {
+    if (tables.length > 0) {
+      const selectContainer = document.createElement("div");
+      selectContainer.setAttribute("id", "select-container");
+      selectContainer.setAttribute("class", "intro-select");
+
+      const selectLabel = document.createElement("label");
+      selectLabel.setAttribute("for", "table-selector");
+      selectLabel.textContent = "Sélection du tableau";
+
+      const selectElement = document.createElement("select");
+      selectElement.setAttribute("name", "table-selector");
+      selectElement.setAttribute("id", "tableSelector");
+      selectElement.setAttribute("class", "table-select");
+
+      selectContainer.appendChild(selectLabel);
+      selectContainer.appendChild(selectElement);
+      selectSection.appendChild(selectContainer);
+
+      tables.forEach((table) => {
+        // Create options for the select element
+        const selectOption = document.createElement("option");
+        selectOption.setAttribute("id", table.tableId);
+        selectOption.setAttribute("class", "select-options");
+        selectOption.textContent = table.tableName;
+
+        selectElement.appendChild(selectOption);
+
+        // Create menu buttons for each table
+        const tableButtonContainer = document.createElement("div");
+        tableButtonContainer.setAttribute("class", "navbar-table-link-block");
+
+        const tableButtonLink = document.createElement("a");
+        tableButtonLink.setAttribute("class", "navbar-table-link");
+        tableButtonLink.setAttribute(
+          "href",
+          "./table.html?id=" + table.tableId
+        );
+        tableButtonLink.setAttribute("title", table.tableName);
+        tableButtonContainer.appendChild(tableButtonLink);
+
+        const tableLogo = document.createElement("i");
+        tableLogo.setAttribute("class", "fa fa-table");
+        tableButtonLink.appendChild(tableLogo);
+
+        const tableTitle = document.createElement("h5");
+        tableTitle.setAttribute("class", "navbar-table-title");
+        tableTitle.textContent = table.tableName;
+        tableButtonLink.appendChild(tableTitle);
+
+        const closeButtonSpan = document.createElement("span");
+        closeButtonSpan.setAttribute("id", "deleteTable-" + table.tableId);
+        closeButtonSpan.setAttribute("class", "navbar-table-link-delete");
+        closeButtonSpan.setAttribute("title", "Supprimer le tableau");
+        tableButtonContainer.appendChild(closeButtonSpan);
+
+        const closeButtonIcon = document.createElement("i");
+        closeButtonIcon.setAttribute(
+          "id",
+          "deleteTable-" + table.tableId + "-icon"
+        );
+        closeButtonIcon.setAttribute(
+          "class",
+          "navbar-table-link-delete-icon fa fa-xmark"
+        );
+        closeButtonSpan.appendChild(closeButtonIcon);
+
+        closeButtonSpan.addEventListener("click", (e) => {
+          const tableToDelete = e.target.id.split("-")[1];
+          deleteTable(tableToDelete);
+          console.log(tableToDelete);
+        });
+
+        document.getElementById("table-list").appendChild(tableButtonContainer);
+      });
+      return selectElement;
+    }
+  }
+}
+
+// Asign the localstorage variable to the select value if none is set
+function setSelectedTable() {
+  selectedTableOption = selectTable.options[selectTable.selectedIndex];
+  console.log(selectedTableOption.value);
+  localStorage.setItem("selected-table", selectedTableOption.id);
+
+  selectTable.options[localStorage.getItem("selected-table")].selected = true;
+
+  selectedTable = tables.find((table) => {
+    return table.tableId === selectedTableOption.id;
+  });
+  selectedTableIndex = tables.indexOf(selectedTable);
+  location.reload();
+}
+
+// Function used to create a new form to the tool
+function createNewTable() {
+  const tableName = document.getElementById("newTableName").value;
+
+  console.log(tableName);
+
+  // We create a unique ID for each table to allow for an easier identification by the code
+  const tableId = tableName.split(" ").join("_") + "_" + Date.now();
+
+  const newTable = {
+    tableName: tableName,
+    tableId: tableId,
+    tableInputs: [],
+    tableData: [],
+  };
+
+  if (Array.isArray(tables)) {
+    tables.push(newTable);
+  } else {
+    tables = [];
+    tables.push(newTable);
+  }
+
+  localStorage.setItem("tables", JSON.stringify(tables));
+}
+
+// Function that handles the deletion of a table
+function deleteTable(idOfTable) {
+  // We look for the table we want to delete in the global tables array
+  const tableToDelete = tables.find((table) => table.tableId === idOfTable);
+  // console.log(tableToDelete);
+  console.log(tables.indexOf(tableToDelete));
+  // Now we register the table's index
+  const tableIndex = tables.indexOf(tableToDelete);
+  tables.splice(tableIndex, 1);
+  localStorage.setItem("tables", JSON.stringify(tables));
+  location.reload();
+}
+
+// Function that allows to create a new input in the main form
+function addInputToForm(event) {
+  event.preventDefault();
+
+  const tableInputs = selectedTable.tableInputs;
+
+  // console.log(selectedTable);
+
+  const newInputName = document.getElementById("newInput");
+  const newInputType = document.getElementById("newInputType");
+  const newInputRequired = document.getElementById("isRequired");
+
+  if (newInputName.value == "") {
+    return (document.getElementById("errorNewInput").innerHTML =
+      "DONNEZ UN NOM AU CHAMP");
+  }
+
+  const newInputObject = {
+    name: newInputName.value,
+    type: newInputType.value,
+    required: newInputRequired.checked,
+  };
+
+  // Now we send the new input data to the JSON file
+
+  // If the table does not have any inputs yet then we create an array in which all of the inputs will be contained
+  if (tableInputs === []) {
+    let newArray = [];
+    newArray.push(newInputObject);
+    tableInputs = newArray;
+    // Now we register the table's index
+    tables.splice(selectedTableIndex, 1, selectedTable);
+    localStorage.setItem("tables", JSON.stringify(tables));
+  } else if (
+    // Else, we check if the exact same input exists in the table. If it's the case we don't add the new one, if it's not we can add it
+    !selectedTable.tableInputs.some(
+      (e) =>
+        e.name === newInputObject.name &&
+        e.type === newInputObject.type &&
+        e.required === newInputObject.required
+    )
+  ) {
+    let newArray = tableInputs.concat([newInputObject]);
+    selectedTable.tableInputs = newArray;
+    tables.splice(selectedTableIndex, 1, selectedTable);
+    console.log(tables);
+    localStorage.setItem("tables", JSON.stringify(tables));
+  } else {
+    alert("Ce champ est déjà présent à l'identique dans ce tableau");
+  }
+
+  location.reload();
+}
+
+function getFormInputs() {
+  if (selectedTable.tableInputs != null) {
+    for (i = 0; i < selectedTable.tableInputs.length; i++) {
+      const label = document.createElement("label");
+      label.setAttribute("for", selectedTable.tableInputs[i].name);
+      label.innerText = selectedTable.tableInputs[i].name;
+
+      const input = document.createElement("input");
+      input.setAttribute("name", selectedTable.tableInputs[i].name);
+      input.setAttribute("type", selectedTable.tableInputs[i].type);
+      input.setAttribute("id", selectedTable.tableInputs[i].name);
+      input.setAttribute("class", "new-input");
+      input.setAttribute("placeholder", selectedTable.tableInputs[i].name);
+      if (selectedTable.tableInputs[i].required === true) {
+        input.setAttribute("required", true);
+      }
+
+      const deleteInput = document.createElement("button");
+      deleteInput.setAttribute("id", input.name + "Delete");
+      deleteInput.setAttribute("class", "button-remove");
+      deleteInput.setAttribute("type", "button");
+      deleteInput.setAttribute("onclick", "deleteInput(" + i + ")");
+      deleteInput.innerText = "X";
+
+      const inputDeleteDiv = document.createElement("div");
+      inputDeleteDiv.appendChild(input);
+      inputDeleteDiv.appendChild(deleteInput);
+
+      const newInputDiv = document.createElement("div");
+      newInputDiv.setAttribute("class", "form-group");
+      newInputDiv.setAttribute("id", input.name + "Group" + i);
+      newInputDiv.appendChild(label);
+      newInputDiv.appendChild(inputDeleteDiv);
+
+      newObjectForm.appendChild(newInputDiv);
+    }
+  }
+}
+
+function deleteInput(index) {
+  let array = selectedTable.tableInputs;
+  array.splice(index, 1);
+  console.log(array);
+  selectedTable.tableInputs = array;
+  tables.splice(selectedTableIndex, 1, selectedTable);
+  localStorage.setItem("tables", JSON.stringify(tables));
+  location.reload();
+}
+
+function displayMainForm() {
+  if (selectedTable) {
+    document.getElementById("newInputFormBlock").style.display = "flex";
+  } else {
+    document.getElementById("newInputFormBlock").style.display = "none";
+  }
+  if (
+    selectedTable.tableInputs != null &&
+    selectedTable.tableInputs.length > 0
+  ) {
+    console.log("OUIOUI");
+    document.getElementById("mainFormBlock").style.display = "flex";
+  } else {
+    document.getElementById("mainFormBlock").style.display = "none";
+  }
+}
+
+function submitMainForm() {
+  let everyKeys = [];
+  let everyValues = [];
+  for (i = 0; i < selectedTable.tableInputs.length; i++) {
+    everyKeys.push(selectedTable.tableInputs[i].name);
+    everyValues.push(
+      document.getElementById(selectedTable.tableInputs[i].name).value
+    );
+  }
+  let newObject = {};
+  everyKeys.forEach((key, i) => (newObject[key] = everyValues[i]));
+  console.log(newObject);
+
+  if (JSON.parse(localStorage.getItem("data")) === null) {
+    let newArray = [];
+    newArray.push(newObject);
+    localStorage.setItem("data", JSON.stringify(newArray));
+  } else {
+    let newArray = JSON.parse(localStorage.getItem("data")).concat([newObject]);
+    console.log(newArray);
+
+    localStorage.setItem("data", JSON.stringify(newArray));
+  }
+}
+
+const selectTable = initPage();
+
+// Initialization of the variables of the selected table
+let selectedTableOption, selectedTable, selectedTableIndex;
+if (tables.length > 0) {
+  if (localStorage.getItem("selected-table")) {
+    if (selectTable.options[localStorage.getItem("selected-table")]) {
+      selectTable.options[
+        localStorage.getItem("selected-table")
+      ].selected = true;
+      selectedTableOption =
+        selectTable.options[localStorage.getItem("selected-table")];
+    } else {
+      selectTable.options[0].selected = true;
+      selectedTableOption = selectTable.options[0];
+      localStorage.setItem("selected-table", selectedTableOption.id);
+    }
+  } else {
+    selectedTableOption = selectTable.options[selectTable.selectedIndex];
+  }
+
+  selectedTable = tables.find((table) => {
+    return table.tableId === selectedTableOption.id;
+  });
+  selectedTableIndex = tables.indexOf(selectedTable);
+
+  selectTable.addEventListener("change", () => {
+    console.log(selectedTable);
+    console.log(selectedTableIndex);
+    setSelectedTable();
+  });
+
+  getFormInputs();
+  displayMainForm();
+}
